0ec7842ed3990c7a428192b24c2f2d5709140366
Jesse <jesse.jedrusiak@gmail.com>
2017-01-04 14:46:41 -0500

    Day 7 Update

--- day7/script7.py
+++ day7/script7.py
@@ -1,21 +1,42 @@
-import re
-def read_ip(datafile):
+def read_ips(datafile):
     with open(datafile) as f:
         ips = zip(*[line.split() for line in f])
     f.close()
     return list(ips[0])
 
-def tls_search():
-    pass
 
-def parse_ip(ip):
-    exterior_abba = re.search(r'(.*)(\w+)(\w+)\2\1(.*)', ip)
-    interior_abba = re.search(r'(.*)\[(\w+)(\w+)\2\1\](.*)', ip)
-    if exterior_abba is not None and interior_abba is None:
-        return True
-    else:
+def splitter(ip, chunksize=6):
+    ip = ' ' + ip + ' '
+    chunks = [ip[i: i+chunksize] for i in range(len(ip))]
+    return [chunk for chunk in chunks if len(chunk) == chunksize]
+
+
+def abba_test(ip):
+    candidates = []
+    for chunk in splitter(ip):
+        a, b, c, d , e, f = chunk
+        if b == e and c == d and b != c:
+            candidates.append(chunk)
+    if len(candidates) == 0:
         return False
+    else:
+        for candidate in candidates:
+            if candidate[0] == '[' and candidate[-1] == ']':
+                return False
+            else:
+                return True
 
-for ip in read_ip('test.txt'):
-    print parse_ip(ip)
 
+def tls_check(ips):
+    results = []
+    for ip in ips:
+        results.append(abba_test(ip))
+    for res in results:
+        print res
+    return sum(results)
+    
+
+def main():
+    print(tls_check(read_ips('input7.txt')))
+
+main()
