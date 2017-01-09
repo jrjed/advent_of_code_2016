5b14843f4bf4b6b40304ccb16167286fe0aa5ebc
Jesse Jedrusiak <jesse.jedrusiak@gmail.com>
2017-01-09 02:16:03 -0500

    Finished Part 2

--- day7/script7.py
+++ day7/script7.py
@@ -9,21 +9,33 @@ def read_ips(datafile):
 
 
 def tls_test(ip):
-    good_abba = re.search(r'([a-z])(?!\1)([a-z])\2\1', ip) 
+    good_abba = re.search(r'([a-z])(?!\1)([a-z])\2\1', ip)
     bad_abba = re.search(r'\[([a-z]*)([a-z])(?!\2)([a-z])\3\2([a-z]*)\]', ip)
-    if good_abba is not None and bad_abba is None:
+    if good_abba and not bad_abba:
         return True
     else:
         return False
 
 
 def ssl_test(ip):
-    pass
+    for i, letter in enumerate(ip):
+        outside = ''.join([s.split(']')[-1] for s in ip.split('[')]).strip()
+        chunk = outside[i: i+3]
+        aba = re.search(r'([a-z])(?!\1)([a-z])\1', chunk)
+        if aba:
+            a, b = aba.group(1, 2)
+            bab = re.search(r'\[([a-z]*)(' + b + a + b + ')([a-z]*)\]', ip)
+            if bab:
+                return True
+    return False
 
 
 def main():
+    # Part 1
     print(sum(map(tls_test, read_ips('input7.txt'))))
-        
+    # Part 2 Guessed 239(too low)
+    print(sum(map(ssl_test, read_ips('input7.txt'))))
+
 
 if __name__ == '__main__':
     main()
