9fdc4a0d907f44804409c91caeee8f639a7f6470
Jesse Jedrusiak <jesse.jedrusiak@gmail.com>
2017-01-11 00:52:09 -0500

    Day 8 Solution

--- day9/script9.py
+++ day9/script9.py
@@ -1,5 +1,6 @@
 import re
 
+
 def read_text(datafile):
     with open(datafile, 'r') as f:
         text = ''.join([line for line in f]).strip()
@@ -7,14 +8,21 @@ def read_text(datafile):
     return text
 
 
+def find_marker(text, index=None):
+    regex = re.compile(r'\((\d+)x(\d+)\)')
+    if index:
+        return regex.match(text, index)
+    else:
+        return re.search(regex.pattern, text)
+
+
 def decompress(text):
     result = ''
-    regex = re.compile(r'\((\d+)x(\d+)\)')
     i = 0
     while i < len(text):
-        match = regex.match(text, i)
+        match = find_marker(text, i)
         if match:
-            start  = int(match.end())
+            start = int(match.end())
             end = start + int(match.group(1))
             n = int(match.group(2))
             chunk = text[start: end]
@@ -23,6 +31,46 @@ def decompress(text):
         else:
             result += text[i]
             i += 1
-    return len(result)
+    return result
 
-print decompress(read_text('input9.txt'))
+
+def full_decompress(text):
+    '''
+    Works for resonably sized strings (e.g. the test cases).
+    Would take too long to run on test input so implemented Peter Norvigs
+    strategy (not shown here) simply to get an answer, but again, this works
+    ... you just need a super computer.
+    '''
+    markers = find_marker(text)
+    result = text
+    while markers:
+        result = decompress(result)
+        markers = find_marker(result)
+    return result
+
+
+def norvig(text):
+    length = 0
+    i = 0
+    while i < len(text):
+        match = find_marker(text, i)
+        if match:
+            interval, n = map(int, match.groups())
+            i = int(match.end())
+            length += n * norvig(text[i: i + interval])
+            i += interval
+        else:
+            length += 1
+            i += 1
+    return length
+
+
+def main():
+    # Part 1
+    print len(decompress(read_text('input9.txt')))
+    # Part 2
+    print norvig(read_text('input9.txt'))
+
+
+if __name__ == '__main__':
+    main()
