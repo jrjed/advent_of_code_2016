fe5e7fdaee6bf81ebdd38a5e46e714208ab1bbf2
Jesse <jesse.jedrusiak@gmail.com>
2017-02-02 15:51:15 -0500

    Completed Puzzle Solution

--- day10/script10.py
+++ day10/script10.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 
 def read_lines(datafile):
@@ -8,43 +9,77 @@ def read_lines(datafile):
     return lines
 
 
-def full(pockets):
-    if len(pockets) == 2:
-        return True
-    else:
-        return False
+def check_pair(bins={}, pair=None):
+    for key in bins.keys():
+        if sorted(pair) == sorted(bins[key]):
+            print key
 
 
-def check_pair():
-    pass
+class BinManager:
+    def __init__(self, bins):
+        self.bins = bins
 
-def robot_process(directions):
-    executed = []
-    bins = {}
-    # add while loop here to keep running over list of directions
-    for i, direction in enumerate(directions):
-        bot = re.search(r'(bot \d+)', direction).group(1)
-        if i not in executed:
-            if bot not in bins.keys():
-                bins[bot] = [] 
-
-            if direction.startswith('value'):
-                value = int(re.search(r'value (\d+)', direction).group(1))
-                if not full(bins[bot]):
-                    bins[bot].append(value)
-                    executed.append(i) # only appending last executed direction?
-                    print i
-
-            if direction.startswith('bot') and not full(bins[bot]):
-                pass
+    def full(self, bot):
+        if len(self.bins[bot]) == 2:
+            return True
         else:
-            continue
-            
-    print bins
-    print executed
-    return bins
+            return False
 
-robot_process(read_lines('test.txt'))
+    def add_value(self, bot, value):
+        if not self.full(bot):
+            self.bins[bot].append(value)
 
-#bins = robot_process(read_lines('test.txt'))
-#print bins
+    def give_value(self, bots=[]):
+        if self.full(bots[0]):
+            self.add_value(bots[1], min(self.bins[bots[0]]))
+            self.add_value(bots[2], max(self.bins[bots[0]]))
+            self.bins[bots[0]] = []
+
+
+def robot_process(directions, pair=[0, 0]):
+    all_bots = {bot[0]: [] for bot in set(re.findall(r'((bot|output) \d+)',
+                                                     ''.join(directions)))}
+    manager = BinManager(all_bots)
+    executed = []
+    while len(executed) < len(directions):
+        for i, direction in enumerate(directions):
+            old = copy.deepcopy(manager.bins)
+            if i not in executed:
+
+                if direction.startswith('value'):
+                    bot = re.search(r'(bot \d+)', direction).group(1)
+                    value = int(re.search(r'value (\d+)', direction).group(1))
+                    manager.add_value(bot, value)
+                    new = manager.bins
+
+                if direction.startswith('bot'):
+                    bots = [b[0] for b in re.findall(r'((bot|output) \d+)',
+                                                     direction)]
+                    manager.give_value(bots)
+                    new = manager.bins
+
+            if old != new:
+                executed.append(i)
+
+            if pair:
+                check_pair(manager.bins, pair)
+
+            else:
+                continue
+    return manager.bins
+
+
+def output_product(bins):
+    num = []
+    for i in xrange(3):
+        num.extend(bins['output ' + str(i)])
+    print num[0] * num[1] * num[2]
+
+
+def main():
+    # Parts 1 and 2
+    output_product(robot_process(read_lines('input10.txt'), [61, 17]))
+
+
+if __name__ == '__main__':
+    main()
